a11669ec6f1ed3245893460cc41a49ff06c7d213
Joel Robertson <robertson0318@neo.rr.com>
2024-10-26 15:05:55 -0400

    Removed unnecessary code from main.go

--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/deadly990/gominesweeper/generation"
-)
-
 func main() {
-	var board = generation.NewBoard(10, 7, 7, 70)
-	fmt.Println(board.Field)
-	fmt.Print("Board validation: ")
-	fmt.Println(generation.Validate(*board))
+
 }

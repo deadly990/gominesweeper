97a77811d4573e96830764d15d0b4b0dd27f5ac7
JJ <Garnerj990@gmail.com>
2024-10-27 17:14:46 -0400

    rendering from game

--- view/view.go
+++ view/view.go
@@ -11,6 +11,7 @@ import (
 type Square struct {
 	Value int
 	Name  string
+	Game  string
 }
 
 func visible(square Square) bool {
@@ -25,7 +26,7 @@ type MainData struct {
 	Mine MineView
 }
 
-func convert(field [][]int) [][]Square {
+func convert(field [][]int, game string) [][]Square {
 	squares := make([][]Square, len(field))
 	for i := range squares {
 		squares[i] = make([]Square, len(field[i]))
@@ -43,13 +44,13 @@ func convert(field [][]int) [][]Square {
 func FromBoard(board generation.Board) MineView {
 	return MineView{
 		Remaining: board.Mines,
-		Squares:   convert(board.Field),
+		Squares:   convert(board.Field, "1"),
 	}
 }
 func FromGame(game game.Game) MineView {
 	return MineView{
 		Remaining: game.Board.Mines,
-		Squares:   convert(game.Revealed),
+		Squares:   convert(game.Revealed, "2"),
 	}
 }
 func Generate() *template.Template {
